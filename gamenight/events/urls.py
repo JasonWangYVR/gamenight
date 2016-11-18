21e73c674ac286216ce6d5bf2f7f6b37d21c8458
jmang <jmang@sfu.ca>
2016-11-17 18:03:29 -0800

    added question detail view, added edit button for questions, began writing for choices, cleaned up code, added comments

--- gamenight/events/urls.py
+++ gamenight/events/urls.py
@@ -11,8 +11,16 @@ urlpatterns = [
 	#event creation
 	url(r'^new$', views.CreateEventView.as_view(),
     name='create',),
+    #edits event
 	url(r'^edit/(?P<pk>\d+)/$', views.EditEventView.as_view(),
     name='editevent',),
+    #creates question
 	url(r'^addq/(?P<pk>\d+)/$', views.CreateQuestionView.as_view(),
     name='addquestion',),
+    #edit question
+    url(r'^editq/(?P<pk>\d+)/$', views.EditQuestionView.as_view(),
+    name='editquestion',),
+    #creates choice
+    url(r'^addq/(?P<pk>\d+)/$', views.CreateChoiceView.as_view(),
+    name='addchoice',),
     ]
