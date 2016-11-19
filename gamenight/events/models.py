ce03dbb806c75ac1a36e1d74dee56b8a1d0f8716
Evan Chisholm <evan.chisholm@gmail.com>
2016-11-18 21:43:04 -0800

    added some sample fixtures for testing

--- gamenight/events/models.py
+++ gamenight/events/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User,Group
-
+from django.utils import timezone
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=140)
