a370856b7ea63487cd968ab0ffed68949fe5e022
Jason Wang <jwa131@sfu.ca>
2016-11-23 17:56:35 -0800

    Added UserProfile, and related things.
    -UserProfile model added into authentication.models.py
    -Added ProfileForm to authentication.forms.py
    -Added create_profile.html to authentication.templates.authentication
    -Added create, view, and edit views for Profile into authentication.views.py
    -Created related URLs for those things.
    -Nothing has been tested yet.

--- gamenight/authentication/models.py
+++ gamenight/authentication/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from gamenight.events.models import Event
 #JASON: This entire model needs to reworked, and it also needs to actually work.
 #       TODO:   -Add ManyToMany for favorite boardgames
 #               -Add ManyToMany for events that the user is attending.
-class GNUser(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     addr_1 = models.CharField(max_length=30)
     addr_2 = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
     prov = models.CharField(max_length=5)
     post_zip = models.CharField(max_length=10)
+    attending_events = models.ManyToManyField(Event)
 
     class Meta:
         abstract = True
+
+    def create_profile(self, user, addr_1, addr_2, city, prov, post_zip):
+        self.user = user
+        self.addr_1 = addr_1
+        self.addr_2 = addr_2
+        self.city = city
+        self.prov = prov
+        self.post_zip = post_zip
+        self.save()
+        return self
--- gamenight/authentication/views.py
+++ gamenight/authentication/views.py
@@ -3,8 +3,9 @@ from django.template import RequestContext, loader
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from authentication.forms import *
-from authentication.models import GNUser
+from authentication.models import UserProfile
 
 def signup(request):
     if not request.user.is_authenticated():
@@ -23,7 +24,6 @@ def signup(request):
                 registered = True
                 template = loader.get_template('authentication/signup.html')
                 context = RequestContext(request, {'registered':registered })
-                #return HttpResponse(template.render(context))
                 return render(request, 'authentication/signup.html', {'registered': registered,})
         else:
             form = UserForm()
@@ -34,7 +34,7 @@ def signup(request):
             'registered': registered,
         })
         #return HttpResponse(template.render(context))
-        return render(request, 'authentication/signup.html', {'form':form, 'registered':registered,})
+        return render(request, 'authentication/signup.html', {'form': form, 'registered': registered})
     else:
         return redirect('authentication:login')
 
@@ -56,20 +56,71 @@ def log_in(request):
         else:
             log_form = LoginForm()
 
-        template = loader.get_template('authentication/login.html')
-        context = RequestContext(request, {
-            'form': log_form,
-            'wrong': wrong,
-        })
+        # template = loader.get_template('authentication/login.html')
+        # context = RequestContext(request, {
+        #     'form': log_form,
+        #     'wrong': wrong,
+        # })
         #return HttpResponse(template.render(context))
         return render(request, 'authentication/login.html', {'form':log_form, 'wrong':wrong,})
     else:
-        return redirect('authentication:profile')
+        try:
+            profile = UserProfile.objects.filter(user=request.user, deleted=False)
+            context = {'user':request.user,'profile':profile}
+        except ObjectDoesNotExist:
+            context = {'user':request.user}
+        return render(request, 'authentication/profile.html', context)
 
 def log_out(request):
     logout(request)
     #return redirect('home:index')
     return redirect('authentication:login')
 
+
 def profile(request):
-    return render(request, 'authentication/profile.html')
+    if request.user.is_authenticated():
+        try:
+            profile = UserProfile.objects.get(user=request.user, deleted=False)
+            context = {
+                'user':request.user,
+                'profile':profile,
+            }
+            return render(request, 'authentication/profile.html', context)
+        except ObjectDoesNotExist:
+            return redirect('authentication:create_profile')
+    else:
+        return redirect('authentication:login')
+
+def create_profile(request):
+    if request.user.is_authenticated():
+        try:
+            profile = UserProfile.objects.get(user=request.user, deleted=False)
+            context = {
+                'user':request.user,
+                'profile':profile,
+            }
+            return render(request, 'authentication/profile.html', context)
+        except ObjectDoesNotExist:
+            form = ProfileForm()
+            profile_created = False
+            if form.is_valid():
+                profile = UserProfile.objects.create_profile(
+                    user = request.user
+                    addr_1 = form.cleaned_data['addr_1'],
+                    addr_2 = form.cleaned_data['addr_2'],
+                    city = form.cleaned_data['city'],
+                    prov = form.cleaned_data['prov'],
+                    post_zip = form.cleaned_data['post_zip'],
+                )
+                profile_created = True
+            context = {
+                'profile_created'=profile_created
+            }
+        template = loader.get_template('authentication/create_profile.html')
+        return render(request, 'authentication/create_profile.html', context)
+    else:
+        return redirect('authentication:login')
+
+#TODO
+def edit_profile(request):
+    pass
