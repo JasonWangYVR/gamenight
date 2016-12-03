031b399afad03d4f0ac31ce2b8d584ad21e30034
Nari Shin <naris@sfu.ca>
2016-12-03 12:32:23 -0800

    fixed nested files and a few event templates

--- gamenight/gamenight/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^signup/$',views.signup, name='signup'),
-    url(r'^login/$', views.log_in, name='login'),
-    url(r'^logout/$', views.log_out, name='logout'),
-    url(r'^profile/$', views.profile, name='profile'),
-]
