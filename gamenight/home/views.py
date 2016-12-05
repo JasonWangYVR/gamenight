2d3786555e546aa69a9b28147b8d2fb93589684c
Aman Deol <asd11@sfu.ca>
2016-12-05 11:57:19 -0800

    forgot about user not logged in home

--- gamenight/home/views.py
+++ gamenight/home/views.py
@@ -36,8 +36,22 @@ def index(request):
 		}
 		return render(request, 'home/index.html', context)
 	else:
+		event_list = Event.objects.filter(private_event=False)
+		# event_list = Event.objects.all()
+		page = request.GET.get('page')
+		paginator = Paginator(event_list, 10)
+		try:
+			events = paginator.page(page)
+		except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+			events = paginator.page(1)
+		except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+			events = paginator.page(paginator.num_pages)
+
 		context = {
 			'user':request.user,
+			'events':events,
 			'search':SearchForm(),
 		}
 		return render(request, 'home/index.html', context)
