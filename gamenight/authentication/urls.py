a714878c4300fcedf9d0817b8de84f404f6d63b1
Jason <jwa131@sfu.ca>
2016-12-04 17:35:51 -0800

    Added edit profile and personal information views and templates and urls

--- gamenight/authentication/urls.py
+++ gamenight/authentication/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     url(r'^login/$', views.log_in, name='login'),
     url(r'^logout/$', views.log_out, name='logout'),
     url(r'^profile/$', views.profile, name='profile'),
+    url(r'^profile/edit-personal/$', views.edit_personal, name='edit_personal'),
+    url(r'^profile/edit-profile/$', views.edit_profile, name='edit_profile'),
     url(r'^create-profile/$', views.create_profile, name='create_profile'),
     url(r'^favourite-list/$', views.favourite_list, name='favourite_list'),
+
 ]
